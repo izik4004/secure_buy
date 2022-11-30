7c14e20dbb617000f76c4eb7164a657234b67782
izik <izik4004@gmail.com>
2022-12-01 00:59:57 +0100

    test

--- contracts/SecureBuy.sol
+++ contracts/SecureBuy.sol
@@ -1,42 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SBuy is ERC721URIStorage {
-
-    // address payable owner 
+contract SecureBuy is ERC721URIStorage {
 
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
-    Counters.Counter private _soldItems;
+
     
     // uint256 listPrice = 0.01 ether;
 
 
     constructor() ERC721("SBuy", "SCB") {}
        
-        // owner = payable(msg.sender);
-
         function mint(string memory tokenURI) public returns (uint256) {
         //Increment the tokenId counter, which is keeping track of the number of minted NFTs
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
         //Mint the NFT with tokenId newTokenId to the address who called createToken
-        _safeMint(msg.sender, newItemId);
+        _mint(msg.sender, newItemId);
 
         //Map the tokenId to the tokenURI (which is an IPFS URL with the NFT metadata)
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }
 
-    function totalSupply() public view return (uint256) {
-        return _tokenIds.current()
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 
 }
