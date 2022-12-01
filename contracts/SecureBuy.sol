ea8bf86e8a7ec44b4cd45ec63bd7ecb48abdf8cd
izik <izik4004@gmail.com>
2022-12-01 21:32:14 +0100

    token contract

--- contracts/SecureBuy.sol
+++ contracts/SecureBuy.sol
@@ -7,18 +7,95 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SecureBuy is ERC721URIStorage {
+    address payable owner;
 
     using Counters for Counters.Counter;
-    
     Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
 
-    
-    // uint256 listPrice = 0.01 ether;
+    uint256 listPrice = 0.01 ether;
+
+    constructor() ERC721("SecureBuy", "SCB") {
+        owner = payable(msg.sender);
+    }
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+
+    mapping(address => ListedToken) private idToListedToken;
+
+    function updateListingPrice(uint256 _listprice) public payable {
+        require(owner == msg.sender, "Only owner can update the listing price");
+        listPrice = _listprice;
+    }
+
+    function getListPrice() public view returns (uint256) {
+        return listPrice;
+    }
+
+    function getLatestIdToListedToken() public view returns (ListedToken memory) {
+        uint256 currentTokenId = _tokenIds.current();
+        return idToListedToken[currentTokenId];
+    }
+
+    getListedForTokenId(uint256 tokenId) public view returns (ListedToken memory) {
+        return idToListedToken[currentTokenId];
+    }
+
+    function getCurrentToken() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        require(msg.value == listPrice, "Send enough ether to list");
+        require(price > 0, "make sure the price isn't negative");
+
+        _tokenIds.increment();
+        uint256 currentTokenId = _tokenIds.current();
+        _safeMint(msg.sender, currentTokenId);
+
+        _setTokenURI(currentTokenId, tokenURI);
+        createListedToken(currentTokenId, price);
+
+        return currentTokenId;
+    }
+
+    function createListedToken(uint256 tokenId, uint256 price) private {
+        idToListedToken[tokenId] = ListedToken(
+            tokenId,
+            payable(address(this)),
+            payable(msg.sender),
+            price,
+            true
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+    }
+
+    function getAllNFTs() public view returns (ListedToken[] memory) {
+        uint nftCount = _tokenIds.current();
+        ListedToken[] memory tokens = new ListedToken[](nftCount);
+
+        uint currentIndex = 0;
+
+        for(uint i=0; i<Count; i++)
+        {
+            unit currentId = i + 1;
+            ListedToken storage currentItem = idToListedToken[currentId];
+            tokens[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+
+        return tokens
+    }
 
 
-    constructor() ERC721("SBuy", "SCB") {}
-       
-        function mint(string memory tokenURI) public returns (uint256) {
+    function mint(string memory tokenURI) public returns (uint256) {
         //Increment the tokenId counter, which is keeping track of the number of minted NFTs
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
@@ -34,5 +111,4 @@ contract SecureBuy is ERC721URIStorage {
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
-
 }
