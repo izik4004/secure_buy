4a199d8cb6e7eaee49ac7e18584e6aa894fdfff9
izik <izik4004@gmail.com>
2022-12-01 01:29:01 +0100

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,71 @@
+import { useState, useRef } from "react";
+import { hero } from "../data/data";
+import { HiOutlineChevronDown } from "react-icons/hi";
+import SellForm from "./SellForm";
+import useClickOutside from "../hooks/useClickOutside";
+import SellNFT from "./SellNFT";
+
+const Hero = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  
+  const ref = useRef(null);
+  useClickOutside(ref, () => {
+    if (modalOpen) {
+      setModalOpen(!modalOpen);
+    }
+  });
+
+  return (
+    <>
+      {/* <div
+        aria-hidden="true"
+        class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+      >
+        <div class="blur-[156px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+        <div class="blur-[156px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+      </div> */}
+
+      <section className="flex items-center md:container md:mx-auto justify-between py-4  rounded-xl">
+        {modalOpen && (
+          // <SellForm setModalOpen={setModalOpen} modalOpen={modalOpen} />
+          <SellNFT setModalOpen={setModalOpen} modalOpen={modalOpen} />
+        
+        )}
+        <section ref={ref} className=" px-6  rounded-2xl bg-gray-50">
+          <div  className="grid  px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl ">
+                Scared of <span className="text-accent">getting scammed </span>{" "}
+                by online vendors?
+              </h1>
+              <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                With securebuy your money is safe through the use of an escrow smart contract which holds the funds till your goods are delivered
+              </p>
+              <a onClick={() => setModalOpen(!modalOpen)}
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              >
+                Sell your products
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center  border border-accent rounded-lg hover:bg-gray-100 focus:ring-4 "
+              >
+               Shop Now
+              </a>
+            </div>
+            <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                alt="mockup"
+              />
+            </div>
+          </div>
+        </section>
+       
+      </section>
+    </>
+  );
+};
+
+export default Hero;
